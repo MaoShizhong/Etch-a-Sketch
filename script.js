6a63152a8b42eacd2facbaeeddaff0de44c797da
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-03-13 04:15:32 +0000

    Create working colour/create grid interactions

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let gridSize = 16;
+let gridColor = "black";
+// create initial grid upon loading
+createGrid(16, "black");
 
-document.addEventListener("DOMContentLoaded", createGrid(gridSize, "black"));
+// change color setting
+const buttonBlack = document.querySelector("#black");
+const buttonRGB = document.querySelector("#rgb");
+buttonBlack.addEventListener("click", () => gridColor = "black");
+buttonRGB.addEventListener("click", () => gridColor = "rgb");
 
-const cellsBlack = document.querySelectorAll(".cell-black");
-for (let i = 0; i < cellsBlack.length; i++) {
-    cellsBlack[i].addEventListener("mouseover", changeColorToBlack);
-}
-
-/* const cellsRGB = document.querySelectorAll(".cell-rgb");
-for (let i = 0; i < cellsRGB.length; i++) {
-    cellsRGB[i].addEventListener("mouseover", changeColorRGB);
-} */
+const create = document.querySelector("#create");
+create.addEventListener("click", clearThenCreateGrid);
 
 
 function createGrid(gridSize, color) {
@@ -26,19 +25,50 @@ function createGrid(gridSize, color) {
             cell.classList.add("cell-black");
             grid.appendChild(cell);
         }
+        allowColorBlack();
     } else {
         for (let i = 0; i < gridSize ** 2; i++) {
             const cell = document.createElement("div");
             cell.classList.add("cell-rgb");
             grid.appendChild(cell);
         }
+        allowColorRGB();
     }
 }
 
-function changeColorToBlack() {
-    this.style.backgroundColor = "black";
+function clearThenCreateGrid() {
+    const error = document.querySelector(".error");
+    let gridSize = parseInt(document.querySelector("#grid-size").value);
+    if (gridSize < 1 || gridSize > 100) {
+        error.textContent = "Please enter a number between 1 - 100!";
+        return;
+    } else {
+        error.textContent = "";
+    }
+    
+    const grid = document.querySelector(".container");
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    createGrid(gridSize, gridColor);
 }
 
-/* function changeColorRGB() {
-    this.style.backgroundColor = "black";
-} */
+function allowColorBlack() {
+    const cellsBlack = document.querySelectorAll(".cell-black");
+    for (let i = 0; i < cellsBlack.length; i++) {
+        cellsBlack[i].addEventListener("mouseover", () => cellsBlack[i].style.backgroundColor = "black");
+    }
+}
+
+function allowColorRGB() {
+    const cellsRGB = document.querySelectorAll(".cell-rgb");
+    for (let i = 0; i < cellsRGB.length; i++) {
+        cellsRGB[i].addEventListener("mouseover", function() {
+            let randomRed = Math.floor(Math.random() * 256);
+            let randomGreen = Math.floor(Math.random() * 256);
+            let randomBlue = Math.floor(Math.random() * 256);
+            this.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`
+            });
+    }
+}
