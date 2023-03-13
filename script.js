0d21f539bf3635209a5b2129834e9987dc3a3670
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-03-13 01:38:50 +0000

    Refactor into createGrid function

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let grid = document.querySelector(".container");
-const gridSize = 16;
-const cellProportion = 100 / gridSize;
-grid.style.gridTemplateRows = `repeat(${gridSize}, ${cellProportion}fr)`;
-grid.style.gridTemplateColumns = `repeat(${gridSize}, ${cellProportion}fr)`;
-for (let i = 0; i < gridSize ** 2; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    grid.appendChild(cell);
+let gridSize = 16;
+
+document.addEventListener("DOMContentLoaded", createGrid(gridSize));
+
+function createGrid(gridSize) {
+    let grid = document.querySelector(".container");
+
+    const cellProportion = 100 / gridSize;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, ${cellProportion}fr)`;
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, ${cellProportion}fr)`;
+
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        grid.appendChild(cell);
+    }
 }
